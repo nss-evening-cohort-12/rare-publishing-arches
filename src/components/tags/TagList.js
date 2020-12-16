5b70ceba7963c7c1d93a3a48c60cc8c42b394a1c
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-12-15 20:00:20 -0600

    tag manager - can create/edit/delete tags

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -1,17 +1,23 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext, useState, useEffect, useRef } from "react"
 import { TagContext } from "./TagProvider"
 import Tag from "./tag"
 
 export const TagList = (props) => {
+  const editTagDialog = useRef()
+  const deleteTagDialog = useRef()
   const { tags, getTags, createTag, updateTag, deleteTag } = useContext(TagContext)
   const [newTag, setNewTag] = useState("")
+  const [editedTag, setEditTag] = useState("")
   const [isEditing, setIsEditing] = useState(false)
   const [editTagId, setEditTagId] = useState(0)
+  const [deletedTagId, setDeletedTagId] = useState(0) 
 
   const handleInput = (e) => {
       if (isEditing) {
-        updateTag({ id: editTagId, label: newTag })
-        .then(setNewTag(""))
+        updateTag({ id: editTagId, label: editedTag })
+        .then(editTagDialog.current.close())
+        .then(setEditTag(""))
+        .then(setIsEditing(false))
       } else {
         createTag({ label: newTag })
         .then(setNewTag(""))
@@ -19,13 +25,16 @@ export const TagList = (props) => {
   }
 
   const editATag = (e) => {
-    setNewTag(e.target.dataset.tagname)
+    setEditTag(e.target.dataset.tagname)
     setEditTagId(e.target.id)
     setIsEditing(true)
+    editTagDialog.current.showModal()
   }
 
   const deleteATag = (e) => {
-    deleteTag(e.target.id)
+    deleteTag(deletedTagId)
+    .then(deleteTagDialog.current.close())
+    .then(setDeletedTagId(0))
   }
 
   useEffect(() => {
@@ -34,17 +43,38 @@ export const TagList = (props) => {
 
   return (
       <div className="d-flex flex-row justify-content-around">
+        <dialog className="dialog dialog--editTag" ref={editTagDialog}>
+              <div className="d-flex flex-column justify-content-around align-items-center">
+                <h4>Edit this tag</h4>
+                <input className="editTagInput form-control mb-5" type="text" placeholder="Add text" value={editedTag} onChange={e => {
+                  setEditTag(e.target.value)
+                }}></input>
+                <div className="d-flex flex-row justify-content-around align-items-center w-100">
+                  <button className="createTag btn btn-outline-primary" onClick={handleInput}>Ok</button>
+                  <button className="btn btn-outline-primary" onClick={e => editTagDialog.current.close()}>Close</button>
+                </div>
+              </div>
+            </dialog>
+            <dialog className="dialog dialog--deleteTag text-center" ref={deleteTagDialog}>
+                <h4>Are you sure you want to delete this tag?</h4>
+                <div className="d-flex flex-row justify-content-around align-items-center w-100">
+                  <button className="deleteTag btn btn-outline-primary" onClick={deleteATag}>Ok</button>
+                  <button className="btn btn-outline-primary" onClick={e => deleteTagDialog.current.close()}>Close</button>
+                </div>
+            </dialog>
         <div className="allTags">
           {
-            tags.map(tag => <Tag key={tag.id} tag={tag} deleteATag={deleteATag} editATag={editATag} />)
+            tags.map(tag => <Tag key={tag.id} tag={tag} deleteTagDialog={deleteTagDialog} setDeletedTagId={setDeletedTagId} editATag={editATag} />)
           }
         </div>
-        <div className="addTagForm d-flex flex-column justify-content-around align-items-center">
-          <h2>Create a new tag</h2>
-          <input className="tagInput" type="text" placeholder="Add text" value={newTag} onChange={e => {
-            setNewTag(e.target.value)
-          }}></input>
-          <button className="createTag" onClick={handleInput}>Create</button>
+        <div> 
+          <div className="addTagForm d-flex flex-column justify-content-around align-items-center">
+            <h2>Create a new tag</h2>
+            <input className="tagInput" type="text" placeholder="Add text" value={newTag} onChange={e => {
+              setNewTag(e.target.value)
+            }}></input>
+            <button className="createTag" onClick={handleInput}>Create</button>
+          </div>
         </div>
       </div>
   )
--- src/components/tags/tag.js
+++ src/components/tags/tag.js
@@ -4,7 +4,11 @@ import "./tag.css"
 export default (props) => (
   <div className="tags">
     <div className="tagicon"><i className="fas fa-cog" id={props.tag.id} data-tagname={props.tag.label} onClick={props.editATag} ></i></div>
-    <div className="tagicon"><i className="far fa-trash-alt" id={props.tag.id} onClick={props.deleteATag}></i></div>
+    <div className="tagicon"><i className="far fa-trash-alt" id={props.tag.id} onClick={e => {
+        props.setDeletedTagId(props.tag.id)
+        props.deleteTagDialog.current.showModal() 
+      }
+    }></i></div>
     <div className="tagname">{props.tag.label}</div>
   </div>
 )
