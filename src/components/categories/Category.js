ad7135308eaf1e63ba203d8849bf557c68d7077f
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-12-17 18:57:18 -0600

    CRUD on Categories

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -4,7 +4,7 @@ import "./Categories.css"
 
 export default (props) => (
   <div className="categories">
-    <div className="categoryicon"><i className="fas fa-cog fa-2x" id={props.category.id} data-tagname={props.category.label} onClick={props.editACategory}></i></div>
+    <div className="categoryicon"><i className="fas fa-cog fa-2x" id={props.category.id} data-categoryname={props.category.label} onClick={props.editACategory}></i></div>
     <div className="categoryicon"><i className="far fa-trash-alt fa-2x" id={props.category.id} onClick={e => {
       props.setDeletedCategoryId(props.category.id)
       props.deleteCategoryModal.current.showModal() 
