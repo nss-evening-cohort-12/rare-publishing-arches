c110bf58b08146a511da502631473be7dcc00276
Nrg51098 <gaikwadrnikhil@gmail.com>
2020-12-19 07:29:57 -0600

    fixed to show the users first name and last name instead of the userID'

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -3,18 +3,21 @@ import "./PostComments.css"
 
 export const Comment = (props) => (
     <div className="col col-4 comment p-5 m-2">
-      
-      <p className="d-inline">{props.comment.content}</p>      
-      <div className="commenticon d-inline">
-        <i className="far fa-trash-alt" id={props.comment.id} onClick={e => {
-        props.setDeletedCommentId(props.comment.id)
-        props.deleteCommentDialog.current.showModal() 
-        }}></i>
+      <div className="row">
+        <p className="col col-8">{props.comment.content}</p> 
+        <div className="commenticon">   
+          <div className="commenticon d-inline">
+            <i className="far fa-trash-alt" id={props.comment.id} onClick={e => {
+              props.setDeletedCommentId(props.comment.id)
+              props.deleteCommentDialog.current.showModal() 
+            }}></i>
+          </div>
+          <div className="commenticon d-inline">
+            <i className="fas fa-cog" id={props.comment.id} data-commentsubject={props.comment.subject} data-commentcontent={props.comment.content} onClick={props.editAComment} ></i>
+          </div>
+        </div>  
       </div>
-      <div className="commenticon d-inline">
-        <i className="fas fa-cog" id={props.comment.id} data-commentsubject={props.comment.subject} data-commentcontent={props.comment.content} onClick={props.editAComment} ></i>
-      </div>
-      <h1>{props.comment.author.id}</h1>
+      <h6 className="m-0">{props.comment.author.user.first_name} {props.comment.author.user.last_name}</h6>
       
     </div>
 )
