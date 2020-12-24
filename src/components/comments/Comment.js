fdf7665b8338cdfd4849dba1900d3e4aca546fd9
Nrg51098 <gaikwadrnikhil@gmail.com>
2020-12-24 11:03:37 -0600

    finished the bug fixes to show subject and datetime with correct formate

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -5,7 +5,7 @@ export const Comment = (props) => (
     <div className="col col-4 comment p-5 m-2">
       
       <div className="row">        
-        <h5 className="col col-8">{props.comment.subject}</h5> 
+        <h5 className="col col-9">{props.comment.subject}</h5> 
         <div className="commenticon">   
           <div className="commenticon d-inline">
             <i className="far fa-trash-alt" id={props.comment.id} onClick={e => {
@@ -20,7 +20,7 @@ export const Comment = (props) => (
       </div>
       <p>{props.comment.content}</p>
       <h6 className="m-0">{props.comment.author.user.first_name} {props.comment.author.user.last_name}</h6>
-      <h6 className="dateComment m-0">{props.comment.created_on}</h6>
+      <h6 className="dateComment m-0">{new Date(props.comment.created_on).toLocaleString()}</h6>
       
     </div>
 )
