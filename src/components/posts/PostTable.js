ae6923b088acb45a94a0e8e2738160c5c40ab0d4
Nick Walters <nswalters.s@gmail.com>
2020-12-24 11:11:57 -0600

    Fix post list to match wireframe and requirements

--- src/components/posts/PostTable.js
+++ src/components/posts/PostTable.js
@@ -1,14 +1,16 @@
-import React, { useState, useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect, useRef } from "react"
 import { Link, useHistory } from 'react-router-dom'
 import { PostContext } from "./PostProvider"
 import Post from "./Post"
 import "./Posts.css"
 
 export const PostTable = () => {
-    const { getPosts, posts, searchTerms } = useContext(PostContext)
+    const { getPosts, posts, searchTerms, releasePost } = useContext(PostContext)
     const history = useHistory();
+    const deletePostModal = useRef();
 
     const [filteredPosts, setFiltered] = useState([])
+    const [postToBeDeleted, setPostToBeDeleted] = useState(0)
 
     // Initialization effect hook -> Go get post data
     useEffect(() => {
@@ -29,6 +31,17 @@ export const PostTable = () => {
 
     return (
         <div>
+            <dialog className="dialog dialog--deletePost" ref={deletePostModal}>
+                <h4>Are you sure you want to delete this post?</h4>
+                <div className="d-flex flex-row justify-content-around align-items-center w-100">
+                    <button className="deletePost btn btn-outline-primary" onClick={() => {
+                        releasePost(postToBeDeleted)
+                            .then(history.push("/posts"))
+                            .then(deletePostModal.current.close())
+                    }}>Ok</button>
+                    <button className="btn btn-outline-primary" onClick={e => deletePostModal.current.close()}>Cancel</button>
+                </div>
+            </dialog>
             <div className="d-flex flex-row justify-content-end">
                 <button className="d-flex flex-row justify-content-center align-items-center post__add btn btn-primary mr-5"
                     onClick={() => history.push("/posts/create")}
@@ -41,6 +54,7 @@ export const PostTable = () => {
                 <table className="table table-bordered table-striped">
                     <thead>
                         <tr>
+                            <th scope="col"></th>
                             <th scope="col">Title</th>
                             <th scope="col">Author</th>
                             <th scope="col">Date</th>
@@ -52,6 +66,15 @@ export const PostTable = () => {
                         {
                             filteredPosts.map(post => (
                                 <tr key={post.id}>
+                                    <div className="d-flex flex-row justify-content-around h-100 align-items-center">
+                                        <Link to={`posts/edit/${post.id}`} ><i className="fas fa-cog"></i></Link>
+                                        <i className="far fa-trash-alt text-danger"
+                                            onClick={() => {
+                                                setPostToBeDeleted(post.id)
+                                                deletePostModal.current.showModal()
+                                            }}
+                                        ></i>
+                                    </div>
                                     <td><Link to={`/posts/${post.id}`}>{post.title}</Link></td>
                                     <td>{post.rareuser && post.rareuser.user.first_name} {post.rareuser && post.rareuser.user.last_name}</td>
                                     <td>{post.publication_date}</td>
