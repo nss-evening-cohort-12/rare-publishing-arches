9dac1c660cc229151f534dcdcea7c5f3387ea427
Phonesalo Sihanorak <psihanorak@gmail.com>
2021-01-14 18:15:23 -0600

    Added Update & Delete in Post & CategoryPost

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect, useRef } from "react"
 import { useHistory } from 'react-router-dom'
 import { PostContext } from "./PostProvider"
 import { AuthContext } from '../auth/AuthProvider'
@@ -6,13 +6,21 @@ import Post from "./Post"
 import "./Posts.css"
 
 export const PostList = (props) => {
-    const { getPosts, posts, searchTerms, getPostsByUserId, getPostsByCurrentUserId } = useContext(PostContext)
+    const { getPosts, posts, searchTerms, getPostsByUserId, getPostsByCurrentUserId, releasePost } = useContext(PostContext)
     const { getUserById } = useContext(AuthContext)  
     const [ userProfile, setUserProfile ] = useState({})
 
-    const history = useHistory();
+    const history = useHistory()
+    const deletePostModal = useRef()
 
     const [filteredPosts, setFiltered] = useState([])
+    const [deletePostId, setDeletePostId] = useState(0)
+
+    const deleteAPost = (id) => {
+        releasePost(deletePostId)
+            .then(setDeletePostId())
+            .then(deletePostModal.current.close())
+    }
 
     // Initialization effect hook -> Go get post data
     useEffect(() => {
@@ -25,7 +33,7 @@ export const PostList = (props) => {
             // get all posts
             getPosts()
         }
-    }, [])
+    })
 
     useEffect(() => {
         posts.sort((a, b) => (a.publication_date > b.publication_date) ? -1 : 1)
@@ -33,7 +41,6 @@ export const PostList = (props) => {
         const validPosts = matchingPosts.filter((post) => (Date.parse(post.publication_date) < Date.now()) && (post.approved === true))
         setFiltered(validPosts)
     }, [searchTerms])
-
 
     useEffect(() => {
         posts.sort((a, b) => (a.publication_date > b.publication_date) ? -1 : 1)
@@ -47,6 +54,7 @@ export const PostList = (props) => {
             .then(setUserProfile)    
       }, [])
 
+
     return (
         <div>
             <div className="d-flex flex-row justify-content-end">
@@ -57,13 +65,20 @@ export const PostList = (props) => {
                     <i className="fas fa-plus ml-4 mr-2"></i>
                 </button>
             </div>
+            <dialog className="dialog dialog--deletePost" ref={deletePostModal}>
+                <h4>Are you sure you want to delete this post?</h4>
+                <div className="d-flex flex-row justify-content-around align-items-center w-100">
+                    <button className="deletePost btn btn-outline-primary" onClick={deleteAPost}>Ok</button>
+                    <button className="btn btn-outline-primary" onClick={e => deletePostModal.current.close()}>Cancel</button>
+                </div>
+            </dialog>
             <div className="posts post__list mt-5 mx-5 px-3">
                 <h2>{
                         props.location && props.location.pathname.includes('/user/posts') 
                             && userProfile.user && `${userProfile.user.first_name} ${userProfile.user.last_name}'s Posts`
                 }</h2>
                 {
-                    filteredPosts.map(post => <Post key={post.id} post={post} />)
+                    filteredPosts.map(post => <Post key={post.id} post={post} setDeletePostId={setDeletePostId} deletePostModal={deletePostModal} />)
                 }
             </div>
         </div>
